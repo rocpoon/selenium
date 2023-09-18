78b5ead0cca8240eabfd915075b8d8969e91d252
kajimao <rocpoon@gmail.com>
2023-09-18 13:25:49 -0700

    Update Alert file

--- Selenium/alertConfirmPromptSelection.py
+++ Selenium/alertConfirmPromptSelection.py
@@ -1,9 +1,10 @@
 import time
 from datetime import datetime
 
-## Basic webdriver and By selector
+## Basic webdriver components (selector and key)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 ## Wait support for explicit wait
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,38 +13,44 @@ from selenium.webdriver.support import expected_conditions as EC
 ## Unique webElement
 from selenium.webdriver.support.select import Select
 
-test_automation = "https://testautomationpractice.blogspot.com/"
 
+TEST_AUTOMATION_URL = "https://testautomationpractice.blogspot.com/"
+
+# Chrome specific options
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
+
+
 
 if __name__ == "__main__":
     driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 3)
-    print(f"Current_url Before get(): {driver.current_url}")
-    driver.get(test_automation)
-    print(f"Current_url After get(): {driver.current_url}")
+    driver.get(TEST_AUTOMATION_URL)
 
     alert_list = ["Alert", "Confirm Box", "Prompt"]
-    alert_button = driver.find_element(By.XPATH, f"//button[text()='{alert_list[1]}']")
+    alert_button = driver.find_element(By.XPATH, f"//button[text()='{alert_list[2]}']")
     alert_button.click()
 
+    # START - Alert Window
     # When an alert is on the webpage, use "switch_to" function on driver
+    # *** NOTE: After accept/dismiss an Alert, driver will automatically switch back to the main/original window
     alert_window = driver.switch_to.alert
+
     ### Messages on Alert window
     print(alert_window.text)
 
-    # ### Input text into prompt text field
-    # alert_window.send_keys("Check it out")
+    ### Input text into prompt text field
+    ALERT_INPUT = "Kitty"
+    alert_window.send_keys(ALERT_INPUT)
 
-    ### Click on Cancel button
-    alert_window.dismiss()
+    # ### Alert button options: accept(Ok)/dismiss(Cancel)
+    alert_window.accept()
+    # alert_window.dismiss()
+    # END - Alert Window
 
-    # ### Click on Ok button
-    # alert_window.accept()
+    # Verify correct alert selection output is displayed in main window
+    demo = driver.find_element(By.XPATH, "//p[@id='demo']")
+    print(demo.text)
+    assert demo.text == f'Hello {ALERT_INPUT}! How are you today?'
 
-    # demo = driver.find_element(By.XPATH, "//p[@id='demo']")
-    # print(demo.text)
-
-    time.sleep(2)
     driver.quit()
